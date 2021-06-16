8800cd3d824bbf7ce5219c9d79e3e396fb54dc19
Mateusz Kulinowski <mat.kulinowski@gmail.com>
2021-06-16 19:11:53 +0200

    Add project files.

---
+++ PSCF-Ethernet/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using PcapDotNet.Core;
+using PcapDotNet.Packets;
+using PcapDotNet.Packets.IpV4;
+using PcapDotNet.Packets.Transport;
+using System.Windows;
+
+namespace PSCF_Ethernet
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ReceiveTraffic_Click(object sender, RoutedEventArgs e)
+        {
+            OfflinePacketDevice selectedDevice = new OfflinePacketDevice(@"D:\studia\Gildia Magów Ognia\PSC-F\PSC-F\inputFile2.pcap");
+
+            // Open the capture file
+            using (PacketCommunicator communicator =
+                selectedDevice.Open(65536,                                  // portion of the packet to capture
+                                                                            // 65536 guarantees that the whole packet will be captured on all the link layers
+                                    PacketDeviceOpenAttributes.Promiscuous, // promiscuous mode
+                                    1000))                                  // read timeout
+            {
+                // Read and dispatch packets until EOF is reached
+                communicator.ReceivePackets(0, DispatcherHandler);
+            }
+        }
+
+        private void DispatcherHandler(Packet packet)
+        {
+            trafficBox.Items.Add(packet.Timestamp.ToString("yyyy-MM-dd hh:mm:ss.fff") + " length:" + packet.Length);
+
+            IpV4Datagram ip = packet.Ethernet.IpV4;
+            UdpDatagram udp = ip.Udp;
+
+            trafficBox.Items.Add(ip.Source + ":" + udp.SourcePort + " -> " + ip.Destination + ":" + udp.DestinationPort + "\n");
+        }
+
+    }
+}
